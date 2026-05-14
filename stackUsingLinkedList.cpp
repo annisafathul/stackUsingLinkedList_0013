037eddc5324942c58f98226731c1811a60414a81
annisafathul <annisafathul18@gmail.com>
2026-05-14 15:21:32 +0700

    push the entered value onto the stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -89,6 +89,16 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
+        switch (choice)
+        {
+            case 1: 
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); 
+            break;
+
+        }
+
     }
 
 
