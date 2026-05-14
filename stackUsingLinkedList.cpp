92cb4e9860e126535700dbc2db45e41eeaa07fb9
annisafathul <annisafathul18@gmail.com>
2026-05-14 15:30:45 +0700

    add invalid menu choice message

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -122,9 +122,12 @@ int main()
             case 4:
             cout << "Exiting program." << endl;
             break;
-
+   
+            default:
+            cout << "invalid choice. Try again." << endl;
+            break;
         }
-
+        cout << endl;
     }
 
 
