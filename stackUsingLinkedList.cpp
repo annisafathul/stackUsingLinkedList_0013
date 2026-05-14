ae740e83678e8bd88627e380076c3b6255d5be66
annisafathul <annisafathul18@gmail.com>
2026-05-14 15:22:59 +0700

    pop the element from the stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -97,6 +97,12 @@ int main()
             stack.push(value); 
             break;
 
+            case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+
         }
 
     }
